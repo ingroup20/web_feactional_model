6d0eb994a471f11b88caaa65c18a82726ce51a03
chuckwang <chuckwang@gatewen.com.tw>
2024-12-30 16:10:52 +0800

    java mail and test.

--- src/main/java/com/t1/demo_web_springboot/DemoWebSpringbootApplication.java
+++ src/main/java/com/t1/demo_web_springboot/DemoWebSpringbootApplication.java
@@ -1,15 +1,27 @@
 package com.t1.demo_web_springboot;
 
+import com.t1.demo_web_springboot.mail.JavaMailService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoWebSpringbootApplication {
 
+    @Autowired
+    JavaMailService javaMailService ;
+
     public static void main(String[] args) {
         SpringApplication.run(DemoWebSpringbootApplication.class, args);
         System.out.println("Hello, Spring Boot!");
-
     }
+
+//    @PostConstruct
+//    public void run() {
+//        String sendTo = "";
+//        String sendFrom = "";
+//        javaMailService.sendMail(sendTo, sendFrom, "orderSendFrom");
+//    }
 
 }
